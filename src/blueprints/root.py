fd90c310d95104ce959a316e418f47be806df5dd
Thomas Voss <mail@thomasvoss.com>
2023-07-19 23:08:46 +0200

    Add a really basic about page

--- src/blueprints/root.py
+++ src/blueprints/root.py
@@ -7,3 +7,8 @@ root_bp = Blueprint("root", __name__, url_prefix="/")
 @root_bp.route("/", methods=["GET"])
 def index() -> str:
 	return flask.render_template("index.html")
+
+
+@root_bp.route("/about", methods=["GET"])
+def about() -> str:
+	return flask.render_template("about.html")
