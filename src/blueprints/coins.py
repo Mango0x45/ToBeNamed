ca60afa17fa7103f9cdc5ca523ddab06c7c12d5c
Thomas Voss <mail@thomasvoss.com>
2023-07-22 01:43:45 +0200

    Use tabs instead of spaces

--- src/blueprints/coins.py
+++ src/blueprints/coins.py
@@ -6,4 +6,4 @@ coins_bp = Blueprint("coins", __name__, url_prefix="/coins")
 
 @coins_bp.route("/", methods=["GET"])
 def index() -> str:
-    return flask.render_template("coins/index.html")
+	return flask.render_template("coins/index.html")
