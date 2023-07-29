93134155b3b98edb3466ba2d168fdca51121aa90
Thomas Voss <mail@thomasvoss.com>
2023-07-29 21:05:46 +0200

    Typehint the ‘_()’ utility function

--- src/util.py
+++ src/util.py
@@ -1,4 +1,9 @@
+from typing import TypeVar
+
+T = TypeVar("T")
+
 # _() is a function that does literally nothing, but we need to wrap our
 # translatable strings in it so that gettext can figure out what needs
 # translating, and build those translation strings for us.
-_ = lambda x: x
+def _(x: T) -> T:
+	return x
