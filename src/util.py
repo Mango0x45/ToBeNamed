f2ebe14dd76b2f0e6384b430dc99706627a7d463
Thomas Voss <mail@thomasvoss.com>
2023-08-04 17:41:30 +0200

    Use a proper doc comment

--- src/util.py
+++ src/util.py
@@ -2,10 +2,13 @@ from typing import TypeVar
 
 T = TypeVar("T")
 
-# _() is a function that does literally nothing, but we need to wrap our
-# translatable strings in it so that gettext can figure out what needs
-# translating, and build those translation strings for us.
+
 def _(x: T) -> T:
+	"""
+	_() is a function that does literally nothing, but we need to wrap our
+	translatable strings in it so that gettext can figure out what needs
+	translating, and build those translation strings for us.
+	"""
 	return x
 
 
