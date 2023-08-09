95ca0df83cabb5914ad8219a706e6705bbf6be23
Thomas Voss <mail@thomasvoss.com>
2023-08-09 08:22:30 +0200

    Create a new CaseInsensitiveString

--- src/xtypes/__init__.py
+++ src/xtypes/__init__.py
@@ -1,3 +1,4 @@
+from .case_insensitive_string import CaseInsensitiveString
 from .country import COUNTRIES, Country
 from .denomination import COIN_DENOMINATIONS, Denomination
 from .locale import EZ_LOCALES, LOCALES, WORLD_LOCALES, Locale
---
+++ src/xtypes/case_insensitive_string.py
@@ -0,0 +1,28 @@
+class CaseInsensitiveString(str):
+	def __str__(self) -> str:
+		return super().__str__().casefold()
+
+	def __repr__(self) -> str:
+		return super().__repr__().casefold()
+
+	def __eq__(self, other: object) -> bool:
+		if type(other) is str:
+			return super().casefold().__eq__(other.casefold())
+		return False
+
+	def __ne__(self, other: object) -> bool:
+		if type(other) is str:
+			return super().casefold().__ne__(other.casefold())
+		return False
+
+	def __lt__(self, other: str) -> bool:
+		return super().casefold().__lt__(other.casefold())
+
+	def __gt__(self, other: str) -> bool:
+		return super().casefold().__gt__(other.casefold())
+
+	def __ge__(self, other: str) -> bool:
+		return super().casefold().__ge__(other.casefold())
+
+	def __le__(self, other: str) -> bool:
+		return super().casefold().__le__(other.casefold())
