39927d77a27938a9026a4ada5251cc843e609902
Thomas Voss <mail@thomasvoss.com>
2023-10-15 14:22:10 +0200

    Simplify the CaseInsensitiveString class

--- src/xtypes/case_insensitive_string.py
+++ src/xtypes/case_insensitive_string.py
@@ -1,30 +1,37 @@
 class CaseInsensitiveString(str):
+	"""
+	A case-insensitive string type.  Comparisons of strings of this type are
+	always performed in a case-insensitive fashion.  This class should be used
+	everywhere where case is not important.
+	"""
+
 	__slots__ = ()
 
 	def __str__(self) -> str:
-		return super().__str__().casefold()
+		return self.casefold()
 
 	def __repr__(self) -> str:
-		return super().__repr__().casefold()
+		return repr(self.casefold())
 
 	def __eq__(self, other: object) -> bool:
 		if type(other) is str:
-			return super().casefold().__eq__(other.casefold())
+			return self.casefold() == other.casefold()
 		return False
 
 	def __ne__(self, other: object) -> bool:
 		if type(other) is str:
-			return super().casefold().__ne__(other.casefold())
+			return self.casefold() != other.casefold()
 		return False
 
 	def __lt__(self, other: str) -> bool:
-		return super().casefold().__lt__(other.casefold())
+		return self.casefold() < other.casefold()
 
 	def __gt__(self, other: str) -> bool:
-		return super().casefold().__gt__(other.casefold())
-
-	def __ge__(self, other: str) -> bool:
-		return super().casefold().__ge__(other.casefold())
+		return self.casefold() > other.casefold()
 
 	def __le__(self, other: str) -> bool:
-		return super().casefold().__le__(other.casefold())
+		return self.casefold() <= other.casefold()
+
+	def __ge__(self, other: str) -> bool:
+		return self.casefold() >= other.casefold()
+
