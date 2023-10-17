988ca73a9abd90611922850ca38bc9b8e3bea6a9
Thomas Voss <mail@thomasvoss.com>
2023-10-17 23:33:30 +0200

    Add the ‘/collecting’ endpoint

--- src/blueprints/collecting.py
+++ src/blueprints/collecting.py
@@ -3,9 +3,19 @@ from http import HTTPMethod
 import flask
 from flask import Blueprint
 
+import util
+
 collecting = Blueprint("collecting", __name__, url_prefix="/collecting")
 
 
 @collecting.route("/", methods=[HTTPMethod.GET])
 def index() -> str:
 	return flask.render_template("collecting/index.html")
+
+
+@collecting.route("/crh", methods=[HTTPMethod.GET])
+def crh() -> str:
+	return flask.render_template(
+		"collecting/crh.html",
+		countries=util.countries_by_locale(),
+	)
