1c8ec5ff03dc34a57c42ff92e38a76ebce746c9c
Thomas Voss <mail@thomasvoss.com>
2023-10-19 11:50:40 +0200

    Support disabling locales

--- src/xtypes/locale.py
+++ src/xtypes/locale.py
@@ -4,12 +4,15 @@ from xtypes.case_insensitive_string import CaseInsensitiveString
 
 
 class Locale:
-	__slots__ = ("iso_639_1", "iso_3166_1", "name")
+	__slots__ = ("iso_639_1", "iso_3166_1", "name", "enabled")
 
-	def __init__(self, iso_639_1: str, iso_3166_1: str, name: str) -> None:
+	def __init__(
+		self, iso_639_1: str, iso_3166_1: str, name: str, enabled: bool
+	) -> None:
 		self.iso_639_1 = CaseInsensitiveString(iso_639_1)
 		self.iso_3166_1 = CaseInsensitiveString(iso_3166_1)
 		self.name = name
+		self.enabled = enabled
 
 	def __eq__(self, other: object) -> bool:
 		match other:
@@ -36,30 +39,30 @@ class Locale:
 
 
 EZ_LOCALES = (
-	Locale("ca", "AD", "català"),
-	Locale("de", "DE", "Deutsch"),
-	Locale("el", "GR", "ελληνικά"),
-	Locale("en", "GB", "English"),
-	Locale("es", "ES", "español"),
-	Locale("et", "EE", "eesti"),
-	Locale("fi", "FI", "suomi"),
-	Locale("fr", "FR", "français"),
-	Locale("ga", "IE", "Gaeilge"),
-	Locale("hr", "HR", "hrvatski"),
-	Locale("it", "IT", "italiano"),
-	Locale("lt", "LT", "lietuvių"),
-	Locale("lv", "LV", "latviešu"),
-	Locale("mt", "MT", "Malti"),
-	Locale("nl", "NL", "Nederlands"),
-	Locale("pt", "PT", "português"),
-	Locale("sk", "SK", "slovenčina"),
-	Locale("sl", "SI", "slovenščina"),
+	Locale("ca", "AD", "català", False),
+	Locale("de", "DE", "Deutsch", False),
+	Locale("el", "GR", "ελληνικά", False),
+	Locale("en", "GB", "English", True),
+	Locale("es", "ES", "español", False),
+	Locale("et", "EE", "eesti", False),
+	Locale("fi", "FI", "suomi", False),
+	Locale("fr", "FR", "français", False),
+	Locale("ga", "IE", "Gaeilge", False),
+	Locale("hr", "HR", "hrvatski", False),
+	Locale("it", "IT", "italiano", False),
+	Locale("lt", "LT", "lietuvių", False),
+	Locale("lv", "LV", "latviešu", False),
+	Locale("mt", "MT", "Malti", False),
+	Locale("nl", "NL", "Nederlands", True),
+	Locale("pt", "PT", "português", False),
+	Locale("sk", "SK", "slovenčina", False),
+	Locale("sl", "SI", "slovenščina", False),
 )
 
 WORLD_LOCALES = (
-	Locale("bg", "BG", "български"),
-	Locale("en", "US", "English\x0A(US)"),
-	Locale("ro", "RO", "română"),
+	Locale("bg", "BG", "български", False),
+	Locale("en", "US", "English\x0A(US)", True),
+	Locale("ro", "RO", "română", False),
 )
 
 LOCALES = EZ_LOCALES + WORLD_LOCALES
